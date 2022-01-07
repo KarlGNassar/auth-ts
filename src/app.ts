fc98daaa538cf2bad40e97750828d7b7ddeb68d0
KarlGNassar <kgnassar01@gmail.com>
2022-01-07 10:59:54 +0200

    setup + user routes

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+require("dotenv").config();
+import express from "express";
+import config from "config";
+import connectToDb from "./utils/connectToDb";
+import log from "./utils/logger";
+import router from "./routes";
+
+const app = express();
+
+app.use(router);
+
+const port = config.get<number>("port");
+
+app.listen(port, () => {
+  log.info(`listening on http://localhost:${port}`);
+  connectToDb();
+});
